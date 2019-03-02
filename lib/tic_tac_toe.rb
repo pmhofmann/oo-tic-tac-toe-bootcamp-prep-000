ada9679187604ec7035838009f714b198158a706
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2019-03-02 13:41:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,7 +55,14 @@ class TicTacToe
     display_board
   end
   
-  
+  def won?
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
+    end
+  end
+
 
 
 end
